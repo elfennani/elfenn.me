d8901863d7e7ba653252c212272bb01c11baee96
Nizar Elfennani <elfennani2002@gmail.com>
2025-11-11 21:36:53 +0100

    Initialize project structure with essential configuration files and components

---
+++ src/app/(frontend)/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles.css'
+import Logo from "@/app/(frontend)/logo";
+
+export default async function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 text-center px-4">
+      <Logo className="max-w-3/4" />
+      <div className="space-y-4 mt-8">
+        <h1 className="mt-6 text-4xl font-bold">Welcome to My Portfolio!</h1>
+        <p className="text-lg text-muted">
+          Currently under construction. Please check back soon for updates.
+        </p>
+      </div>
+    </div>
+  )
+}
