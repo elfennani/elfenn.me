0a5976440f08bf7c575b3599865cfe88534dcd00
Nizar Elfennani <elfennani2002@gmail.com>
2025-11-12 23:24:57 +0100

    Update layout and styles for personal portfolio, including font integration and metadata changes

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import './styles.css'
+import React from "react"
+import "./styles.css"
+import { Metadata } from "next"
+import { Inter, Space_Grotesk } from "next/font/google"
 
-export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  weight: "variable",
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-space-grotesk",
+  weight: ["400", "700"],
+})
+
+export const metadata: Metadata = {
+  description:
+    "Nizar Elfennani's personal portfolio showcasing projects, skills, and contact information.",
+  title: "Nizar Elfennani - Portfolio",
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
+  console.log(inter.variable, spaceGrotesk.variable)
+
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${inter.variable} ${spaceGrotesk.variable} font-sans bg-white text-black dark:bg-black dark:text-white`}
+      >
         <main>{children}</main>
       </body>
     </html>
